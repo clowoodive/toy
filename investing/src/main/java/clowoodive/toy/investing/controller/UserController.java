50b08f95b5ffb199bda85db7f25e8c24f767f8bc
clowoodive <clowoodive@gmail.com>
2023-03-29 18:06:33 +0900

    refactor: 컨트롤러 분리

--- investing/src/main/java/clowoodive/toy/investing/controller/UserController.java
+++ investing/src/main/java/clowoodive/toy/investing/controller/UserController.java
@@ -2,9 +2,9 @@ package clowoodive.toy.investing.controller;
 
 import clowoodive.toy.investing.dto.ProductDto;
 import clowoodive.toy.investing.dto.UserProductDto;
-import clowoodive.toy.investing.service.InvestingService;
 import clowoodive.toy.investing.error.InvestingException;
 import clowoodive.toy.investing.error.ResultCode;
+import clowoodive.toy.investing.service.InvestingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,25 +13,20 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/investing", produces = MediaType.APPLICATION_JSON_VALUE)
-public class InvestingController {
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
     public final static String HEADER_USER_ID_KEY = "x-user-id";
 
     private final InvestingService investingService;
 
     @Autowired
-    public InvestingController(InvestingService investingService) {
+    public UserController(InvestingService investingService) {
         this.investingService = investingService;
     }
 
-    @GetMapping("/products")
-    public List<ProductDto> getProducts() {
-        return investingService.getProductList();
-    }
 
-    @PostMapping("/user/products/{product_id}/investing-amount/{investing_amount}")
-    public void investProduct(@RequestHeader Map<String, String> headers, @PathVariable("product_id") int productId,
-                              @PathVariable("investing_amount") long investingAmount) {
+    @GetMapping("/products")
+    public List<UserProductDto> getUserProducts(@RequestHeader Map<String, String> headers) {
         String userIdStr = headers.get(HEADER_USER_ID_KEY);
         if (userIdStr == null || userIdStr.isEmpty())
             throw new InvestingException(ResultCode.NotFoundUserId, "no user_id");
@@ -40,12 +35,6 @@ public class InvestingController {
         if (userId <= 0)
             throw new InvestingException(ResultCode.BadUserId, "invalid user_id");
 
-        if (productId <= 0)
-            throw new InvestingException(ResultCode.BadProductId, "invalid product_id");
-
-        if (investingAmount <= 0)
-            throw new InvestingException(ResultCode.BadAmount, "invalid amount");
-
-        investingService.investProduct(userId, productId, investingAmount);
+        return investingService.getUserProductList(userId);
     }
 }
